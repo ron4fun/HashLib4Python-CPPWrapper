3ab15f388b85f68890c9a1ef4fa2bc75d186c938
Mbadiwe Nnaemeka Ronald <ron2tele@gmail.com>
2018-10-16 00:41:54 +0100

    Removed old files

--- HashLib/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(ext_modules = cythonize(
-           "HashFactory.pyx",               # our Cython source
-           sources=["HashLibWrapper.cpp"],  # additional source file(s)
-           language="c++",             # generate C++ code
-      ))
